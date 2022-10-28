12d4c1190545504dbb3e1fb43d1ce7ab11dff7b9
Alexey Britvin <britvinaps@gmail.com>
2022-10-28 19:18:35 +0300

    bug

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,6 +1,6 @@
 import MainPage from "./mainPage";
 import ComicsPage from "./comicsPage";
 import Page404 from "./404";
-import SingleComicPage from "./SingleComicPage";
+import SingleComicPage from "./SingleComicLayout";
 
 export {MainPage, ComicsPage, Page404, SingleComicPage};
